8638214a1f6830fa67a6c9a22be4362be2d59fc8
Ilyes <h2553214@gmail.com>
2025-09-20 15:39:14 +0200

    Implémente exigence 1 — hello() (Fixes #1)

---
+++ tracabilite/src/main/java/fr/uha/iut/Hello.java
@@ -0,0 +1,14 @@
+package fr.uha.iut;
+
+public class Hello {
+
+    // Méthode sans paramètre
+    public static String hello() {
+        return "Hello World!";
+    }
+
+    // Méthode avec paramètre
+    public static String hello(String name) {
+        return name;
+    }
+}
